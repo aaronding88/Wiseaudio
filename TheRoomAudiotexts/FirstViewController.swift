954670eb097960d695762d4d70d4b6ed26fbf7d1
aaronding88 <aaronding@aarons-mbp.gateway.pace.com>
2015-05-10 18:15:49 -0700

    Initial Commit

---
+++ TheRoomAudiotexts/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  TheRoomAudiotexts
+//
+//  Created by Aaron Ding on 5/10/15.
+//  Copyright (c) 2015 Aaron Ding. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
